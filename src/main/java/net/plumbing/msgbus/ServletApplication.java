2a0b113a09eb27eac2979c9bd9076ddc1f494525
Yurip <yuripz@gmail.com>
2025-03-22 11:44:09 +0300

    set date in ApplicationName for sync commit only

--- src/main/java/net/plumbing/msgbus/ServletApplication.java
+++ src/main/java/net/plumbing/msgbus/ServletApplication.java
@@ -51,7 +51,7 @@ public class ServletApplication implements CommandLineRunner {
     @Autowired
     public TelegramProperties telegramProperties;
 
-    public static final String ApplicationName="*Receiver_BUS* v.5.03.172";
+    public static final String ApplicationName="*Receiver_BUS* v.5.03.22";
     public static String propJDBC;
     public static String propExtJDBC;
 
