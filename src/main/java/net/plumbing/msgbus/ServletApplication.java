c106ab82f758965572d126caeaca5958d6ea3364
Yurip <yuripz@gmail.com>
2025-07-22 16:01:11 +0300

    outSerializer.setOutputProperty(Serializer.Property.OMIT_XML_DECLARATION, "yes");

--- src/main/java/net/plumbing/msgbus/ServletApplication.java
+++ src/main/java/net/plumbing/msgbus/ServletApplication.java
@@ -51,7 +51,7 @@ public class ServletApplication implements CommandLineRunner {
     @Autowired
     public TelegramProperties telegramProperties;
 
-    public static final String ApplicationName="*Receiver_BUS* v.5.07.17saX";
+    public static final String ApplicationName="*Receiver_BUS* v.5.07.18saX";
     public static String propJDBC;
     public static String propExtJDBC;
 
